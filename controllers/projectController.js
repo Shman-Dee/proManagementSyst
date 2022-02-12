7988f2ad48ce7b6518f589fcba548a6449ee7ff7
Andrew McInally <realfunnyestate@yahoo.com>
2022-02-12 11:00:58 -0700

    fixed tasks

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -43,16 +43,29 @@ module.exports = {
           id: req.params.projectId,
         },
         attributes: ["id", "projectName"],
-        include: [
-          {
-            model: Task,
-            attributes: ["id", "taskName"],
-          },
-        ],
+        // include: [
+        //   {
+        //     model: Task,
+        //     attributes: ["id", "taskName","createdAt"],
+        //     order: ["createdAt", "DESC"],
+
+        //   },
+        // ],
       });
+      const tasksData = await Task.findAll({
+        where: {
+          projectId: req.params.projectId,
+        },
+        order: [["createdAt", "DESC"]],
+        
+      });
+      const tasks = tasksData.map((task) => task.get({ plain: true }));
+      
       const project = projectData.get({ plain: true });
+      
       res.render("singleProject", {
         project,
+        tasks,
       });
     } catch (error) {
       res.json(error);
