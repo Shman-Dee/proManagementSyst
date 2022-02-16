bbde377bd6fb673116f7247ecf32bf169adb2536
Andrew McInally <realfunnyestate@yahoo.com>
2022-02-16 11:29:23 -0700

    added new styling

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,11 @@
 module.exports = {
-    project_tease: str => {
-		const first100 =  str.slice(0, 100);
-		return first100;
-	}
-}
+  project_tease: (str) => {
+    const first100 = str.slice(0, 100);
+    return first100;
+  },
+  format_date: (date) => {
+    return `${new Date(date).getMonth() + 1}/${new Date(
+      date
+    ).getDate()}/${new Date(date).getFullYear()}`;
+  },
+};
