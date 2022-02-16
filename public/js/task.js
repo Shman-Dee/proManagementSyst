c6219f28fd5d79c5036fcf36dd21fb757a8812bb
Jenya.Seletsky <js646@me.com>
2022-02-15 19:34:36 -0700

    added footer logo and errors

--- public/js/task.js
+++ public/js/task.js
@@ -1,13 +1,25 @@
 const $taskName = document.querySelector('#taskName');
 const $taskDesc = document.querySelector('#taskDesc');
 const $createTaskBtn = document.querySelector('#createTaskBtn');
+const $error = document.querySelector("#error");
 
 async function createTask(e) {
     e.preventDefault();
     const projectId = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
-    const response = await fetch("/tasks", {
+
+    console.log($taskDesc.value);
+    if ($taskDesc.value && $taskName.value) {
+        console.log($taskDesc.value && $taskName.value);
+        if ($taskDesc.value.length < 20) {
+          return ($error.textContent = "Please provide at least 20 characters in the description");
+        }
+        if ($taskName.value.length < 4) {
+          return ($error.textContent = "Please provide at least 4 characters for task name");
+        }
+    
+    await fetch("/tasks", {
         method: "post",
         headers: {
             'Accept': 'application/json',
@@ -20,10 +32,17 @@ async function createTask(e) {
             taskDesc: $taskDesc.value,
             projectId
         })
-    });
-    window.location.href = `/projects/${projectId}`;
+    })
+.then((response) => {
+  window.location.href = `/projects/${projectId}`;
+})
+.catch((error) => {
+  $error.textContent = error.responseJSON.error;
+});
+} else {
+    $error.textContent = "Please provide a project name and description";
+  }
 }
-
 
 
 $createTaskBtn.addEventListener('click', createTask);
